35d6482b58778c7f505a4afb0cc1df4f4f6e988b
Hey guys! Im João Ariza <168240843+JoaoAriza@users.noreply.github.com>
2026-05-04 23:34:27 -0300

    Update-- Adição de dependencias

---
+++ src/main/java/com/joao/cyberaudit/model/TlsDetails.java
@@ -0,0 +1,14 @@
+package com.joao.cyberaudit.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+
+public class TlsDetails {
+    private String negotiatedProtocol;
+    private String cipherSuite;
+    private boolean weakProtocol;
+    private String message;
+}
